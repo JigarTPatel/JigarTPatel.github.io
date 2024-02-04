ae0c25d0fd411b33ef412e94199fe9e494748c31
CSE-JigarP <143644097+CSE-JigarP@users.noreply.github.com>
2024-02-04 14:09:58 -0500

    a

---
+++ home/csce242/assignments/07/script.js
@@ -0,0 +1,25 @@
+const changePic = () => {
+    
+    const messageP = document.getElementById("swap");
+    messageP.src="images/img2.png";
+    
+};
+
+const addStar = () => {
+    console.log("stars");
+    const playground = document.getElementById("star-playground");
+   playground.innerHTML += "<img src= 'images/star.png'/>";
+}
+
+
+document.getElementById('rotationSlider').addEventListener('input', function() {
+    const rotationValue = this.value;
+    document.getElementById('rotatingImage').style.transform = `rotate(${rotationValue}deg)`;
+});  
+
+
+
+document.getElementById("swap").onclick = changePic;
+document.getElementById("star-column").onclick=addStar;
+
+
