a3170bd1d733bb7bd121635b872b54f8043d4a9e
CSE-JigarP <143644097+CSE-JigarP@users.noreply.github.com>
2024-02-11 13:37:48 -0500

    a

--- home/csce242/assignments/08/script.js
+++ home/csce242/assignments/08/script.js
@@ -6,11 +6,13 @@ function toggleMenu() {
 function showYogaSlider() {
     document.querySelector('.yoga-slider').style.display = 'block';
     document.querySelector('.enter-command').style.display = 'none';
+    document.getElementById('optionsExplanation').innerText = 'Yoga Slider: Slide through different yoga images.';
 }
 
 function showEnterCommand() {
     document.querySelector('.enter-command').style.display = 'block';
     document.querySelector('.yoga-slider').style.display = 'none';
+    document.getElementById('optionsExplanation').innerText = 'Enter Command: Type "a" to play basketball, "b" to read a book, or "c" to shovel up some dirt.';
 }
 
 function changeImage(value) {
@@ -22,9 +24,18 @@ function checkCommand() {
     var commandInput = document.getElementById('commandInput').value.toLowerCase();
     var commandResult = document.getElementById('commandResult');
 
-    if (commandInput === 'command1' || commandInput === 'command2' || commandInput === 'command3') {
-        commandResult.innerHTML = "Image by <a href='https://www.freepik.com/free-vector/hand-drawn-stickman-collection_20856908.htm#query=stick%20figure&position=1&from_view=keyword&track=ais&uuid=12d17ea3-cffc-4386-9be4-ff9dc4b5edc2'>Freepik</a>";
-    } else {
-        commandResult.innerHTML = "Invalid command. Image by <a href='https://www.freepik.com/free-vector/hand-drawn-stickman-collection_20856908.htm#query=stick%20figure&position=1&from_view=keyword&track=ais&uuid=12d17ea3-cffc-4386-9be4-ff9dc4b5edc2'>Freepik</a>";
+    switch (commandInput) {
+        case 'a':
+            commandResult.innerHTML = "Playing basketball image: <img src='images/image1.jpg' alt='Playing Basketball'>";
+            break;
+        case 'b':
+            commandResult.innerHTML = "Reading a book image: <img src='images/image2.jpg' alt='Reading a Book'>";
+            break;
+        case 'c':
+            commandResult.innerHTML = "Playing Soccer: <img src='images/image3.jpg' alt='Shoveling up Dirt'>";
+            break;
+        default:
+            commandResult.innerHTML = "Invalid command. Image by <a href='https://www.freepik.com/free-vector/hand-drawn-stickman-collection_20856908.htm#query=stick%20figure&position=1&from_view=keyword&track=ais&uuid=12d17ea3-cffc-4386-9be4-ff9dc4b5edc2'>Freepik</a>";
+            break;
     }
 }
