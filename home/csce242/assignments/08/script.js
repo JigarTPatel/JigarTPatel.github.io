68a221a69dbac4e346aa8a7d4547d7a88c810030
CSE-JigarP <143644097+CSE-JigarP@users.noreply.github.com>
2024-02-11 13:46:34 -0500

    a

--- home/csce242/assignments/08/script.js
+++ home/csce242/assignments/08/script.js
@@ -26,13 +26,13 @@ function checkCommand() {
 
     switch (commandInput) {
         case 'a':
-            commandResult.innerHTML = "Playing basketball image: <img src='images/image1.jpg' alt='Playing Basketball'>";
+            commandResult.innerHTML = "Playing basketball image: <img src='images/Image1.jpg' alt='Playing Basketball'>";
             break;
         case 'b':
-            commandResult.innerHTML = "Reading a book image: <img src='images/image2.jpg' alt='Reading a Book'>";
+            commandResult.innerHTML = "Reading a book image: <img src='images/Image2.jpg' alt='Reading a Book'>";
             break;
         case 'c':
-            commandResult.innerHTML = "Playing Soccer: <img src='images/image3.jpg' alt='Shoveling up Dirt'>";
+            commandResult.innerHTML = "Playing Soccer: <img src='images/Image3.jpg' alt='Playing Soccer'>";
             break;
         default:
             commandResult.innerHTML = "Invalid command. Image by <a href='https://www.freepik.com/free-vector/hand-drawn-stickman-collection_20856908.htm#query=stick%20figure&position=1&from_view=keyword&track=ais&uuid=12d17ea3-cffc-4386-9be4-ff9dc4b5edc2'>Freepik</a>";
